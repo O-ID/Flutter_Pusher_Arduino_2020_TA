92288de2d8d9c2e9aaabb2db925834a343545a47
O-ID <odydahary1@gmail.com>
2020-07-18 12:41:23 +0700

    Update control.dart

--- lib/control.dart
+++ lib/control.dart
@@ -2,7 +2,7 @@
 
 import 'dart:convert';
 
-import 'package:pusher/pusher.dart';
+// import 'package:pusher/pusher.dart';
 import 'package:http/http.dart' as http;
 
 class Customer {
@@ -18,9 +18,9 @@ class Customer {
 }
 
 Future<void> mmain(Map data) async {
-  List list = List();
-  Pusher pu = new Pusher('1028730', 'e2b086b799d195c9cf48',
-      'bff0d130e00935b3d2bd', PusherOptions(cluster: 'ap1'));
+  // List list = List();
+  // Pusher pu = new Pusher('1028730', 'e2b086b799d195c9cf48',
+  //     'bff0d130e00935b3d2bd', PusherOptions(cluster: 'ap1'));
   // Map data = {'"$ind"': tf};
   // createAlbum(data);
   // print(json.encode(data));
